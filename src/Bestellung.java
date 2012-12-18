3ef9532e41b1849940897e556728bdebc4d6f8cb
Me <me@me-Lappy>
2012-12-18 20:05:33 +0100

    bestellung done

--- src/Bestellung.java
+++ src/Bestellung.java
@@ -27,8 +27,38 @@ public class Bestellung {
 	
 	public Keksdose getBestellung() {
 		Keksdose keksdose = new Keksdose();
-		
-	//TODO: Bestellung
+		// another way of solving this would be reflection, and dynamically load <Kekstype>backmachine
+		// but then we would loose type safety
+		for(Position p : positionen) {
+			Keks k = null;
+			Keksbackmaschine b = null;
+			switch (p.getForm()) {
+			case Mond:
+				b = new MondKeksBackmaschine();
+				break;
+			case Rund:
+				b = new RundKeksBackmaschine();
+				break;
+			case Weihnachtsmann:
+				b = new WeihnachtsmannKeksBackmaschine();
+				break;
+			default:
+				break;
+			}
+			if(b == null)
+				continue;
+			
+			b.setTeigart(p.getTeigart());
+			k = b.backe();
+			
+			if(p.getFuellung() != null) {
+				DoppelKeksBackmaschine db = new DoppelKeksBackmaschine();
+				db.setVorlage(k);
+				db.setFuellung(p.getFuellung());
+				k = db.backe();
+			}
+			keksdose.addKeks(k);
+		}
 		return keksdose;
 	}
 }
